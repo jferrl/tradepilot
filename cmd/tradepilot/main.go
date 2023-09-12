505a80af92a52db1df8de667e54a93159ed8a891
Jorge Ferrero Linacero <jorgeferrerolinacero@gmail.com>
2023-09-12 13:49:42 +0200

    chore: simple script

--- cmd/tradepilot/main.go
+++ cmd/tradepilot/main.go
@@ -1,4 +1,87 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/jferrl/go-kraken"
+	"github.com/sdcoffey/big"
+	"github.com/sdcoffey/techan"
+)
+
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*1)
+	defer cancel()
+
+	c := kraken.New(nil).
+		WithAuth(
+			kraken.Secrets{},
+		)
+
+	st, err := c.Market.OHCLData(ctx, kraken.OHCLDataOpts{
+		Pair:     kraken.XXBTZUSD,
+		Interval: 21600,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	dataset := make([][]string, len(st.Pair))
+	for i, v := range st.Pair {
+		dataset[i] = []string{
+			strconv.FormatInt(v.Time(), 10),
+			v.Open(),
+			v.Close(),
+			v.High(),
+			v.Low(),
+			v.Volume(),
+		}
+	}
+
+	series := techan.NewTimeSeries()
+	for _, datum := range dataset {
+		start, _ := strconv.ParseInt(datum[0], 10, 64)
+		period := techan.NewTimePeriod(time.Unix(start, 0), time.Hour*24)
+
+		candle := techan.NewCandle(period)
+		candle.OpenPrice = big.NewFromString(datum[1])
+		candle.ClosePrice = big.NewFromString(datum[2])
+		candle.MaxPrice = big.NewFromString(datum[3])
+		candle.MinPrice = big.NewFromString(datum[4])
+
+		series.AddCandle(candle)
+	}
+
+	closePrices := techan.NewClosePriceIndicator(series)
+
+	indicator := techan.NewEMAIndicator(closePrices, 10) // Create an exponential moving average with a window of 10
+
+	// record trades on this object
+	record := techan.NewTradingRecord()
+
+	entryConstant := techan.NewConstantIndicator(30)
+	exitConstant := techan.NewConstantIndicator(10)
+
+	// Is satisfied when the price ema moves above 30 and the current position is new
+	entryRule := techan.And(
+		techan.NewCrossUpIndicatorRule(entryConstant, indicator),
+		techan.PositionNewRule{})
+
+	// Is satisfied when the price ema moves below 10 and the current position is open
+	exitRule := techan.And(
+		techan.NewCrossDownIndicatorRule(indicator, exitConstant),
+		techan.PositionOpenRule{})
+
+	strategy := techan.RuleStrategy{
+		UnstablePeriod: 10, // Period before which ShouldEnter and ShouldExit will always return false
+		EntryRule:      entryRule,
+		ExitRule:       exitRule,
+	}
+
+	ok := strategy.ShouldEnter(0, record)
+
+	fmt.Println(ok)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/jferrl/tradepilot
 
 go 1.21
+
+require (
+	github.com/sdcoffey/big v0.7.0
+	github.com/sdcoffey/techan v0.12.1
+)
+
+require github.com/google/go-querystring v1.1.0 // indirect
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jferrl/go-kraken v1.0.1
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
